cf6127bf3e406fdc668f42e538f79f3423831719
Michael Franz <mfr@trifork.com>
2019-12-05 21:42:12 +0100

    Updated version and test app

--- app/src/main/java/io/humio/humiotest/App.java
+++ app/src/main/java/io/humio/humiotest/App.java
@@ -8,6 +8,6 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        HumioLogger.with(this, "https://driver-tracking-app.humio.com/api/v1/dataspaces/driver-tracking-app/", "OauPG1UDwUxkfwIDBH8mBIM0qLQIqiKi14TFAgKou5FU", true);
+        HumioLogger.with(this, "URL", "TOKEN", true);
     }
 }
--- app/src/main/java/io/humio/humiotest/MainActivity.java
+++ app/src/main/java/io/humio/humiotest/MainActivity.java
@@ -16,6 +16,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendTest(View view) {
-        HumioLog.d("distance=35.2276712180607 duration=84.5346269607544");
+        HumioLog.d("KEY=35.2276712180607 VALUE=84.5346269607544");
     }
 }
